3a3ac838be25b923fe3fdae1222bf574e8a63456
Nat <nata.gnn@gmail.com>
2017-07-06 00:30:59 +1000

    1

--- test.py
+++ test.py
@@ -1,1 +1,13 @@
-test
+# To access interpreter
+python
+
+# To print an array of all the arguments passed on the CLI [as a script]
+# From a command line it would run as: python test.py "hello"
+import sys
+print sys.argv
+
+# To print the second argument, which is "hello"
+# Doesn't matter if you use hello, "hello" or 'hello', result is just printing 
+import sys
+print sys.argv[1]
+
