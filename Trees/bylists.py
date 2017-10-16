e2d5fa22f8ae2ccdb43a229115c264a82e28c276
Nat <nata.gnn@gmail.com>
2017-10-16 17:40:26 +0300

    p1

--- Trees/bylists.py
+++ Trees/bylists.py
@@ -1,1 +1,64 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+
+
+#myTree = [ 'a', ['b', ['d', [], []], ['e', [], []]], ['c', ['f',[],[]], []]]
+
+myTree = [ 
+	'a',  #root  
+	['b', #left subtree 
+		['d', [], []], #left subtree of the left subtree with 2 empty children
+		['e', [], []]],  #right subtree of the left subtree with 2 empty children 
+	['c', #right subtree 
+		['f',[],[]], #left subtree of the right subtree with 2 empty children
+		[]]    #right subtree of the right subtree with 2 empty children
+	]
+
+
+print (myTree)
+print 'root  =  ', myTree[0]
+print 'left subtree  =  ', myTree[1]
+print 'right subtree  =  ', myTree[2]
+
+
+def BinaryTree(r):
+	return [r,[],[]]
+
+def insertLeft(root, newBranch):
+	t = root.pop(1) #pops root[1] which is the left child of the tree
+	if len(t) > 1:
+		root.insert(1, [newBranch,t,[]]) #Inserts left child newBranch and pushes previously existing child "t" as a left child of newBranch
+	else:
+		root.insert(1, [newBranch, [], []]) #Inserts an item at a given position - 1
+	return root 
+
+def insertRight(root, newBranch):
+	t = root.pop(2) #pops root[2] which is the right child of the tree
+	if len(t) > 1:
+		root.insert(2, [newBranch,[], t]) #Inserts right child newBranch and pushes previously existing child "t" as a right child of newBranch
+	else:
+		root.insert(2, [newBranch, [], []]) #Inserts an item at a given position - 2
+	return root 
+
+def getRootVal(root):
+	return root[0]
+
+def setRootVal(root, newValue):
+	root[0] = newValue
+
+def getLeftChild(root):
+	return root[1]
+
+def getRightChild(root):
+	return root[2]
+
+r = BinaryTree(3)
+insertLeft(r,4)
+insertLeft(r,5)
+insertRight(r,6)
+insertRight(r,7)
+l = getLeftChild(r)
+print l
+
+
+
+
---
+++ Trees/parse_tree.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+from pythonds.basic.stack import Stack
+from pythonds.trees.binaryTree import BinaryTree
+import operator
+
+"""
+Parsing mathematical expression.
+Rules:
+-If the current token is a '(', add a new node as the left child of the current node, and descend to the left child.
+-If the current token is in the list ['+','-','/','*'], set the root value of the current node to the operator represented by the current token. Add a new node as the right child of the current node and descend to the right child.
+-If the current token is a number, set the root value of the current node to the number and return to the parent.
+-If the current token is a ')', go to the parent of the current node.
+
+A simple solution to keeping track of parents as we traverse the tree 
+is to use a stack. Whenever we want to descend to a child of the current 
+node, we first push the current node on the stack. When we want to return 
+to the parent of the current node, we pop the parent off the stack.
+"""
+
+#fpexp (type str) is our initial statement to parse
+
+def buildParseTree(fpexp):
+	fplist = fpexp.split()  #splitting a string of expression into list 
+	#(type list)
+	pStack = Stack()  #create empty stack 
+	#(type 'instance')
+	eTree = BinaryTree('')  #top root value 
+	#(type 'instance')
+	pStack.push(eTree)  #pushed current root into the stack
+	print pStack.size()
+	currentTree = eTree  #current position at a time
+	#(type 'instance')
+
+	for i in fplist:
+		if i == '(':
+			currentTree.insertLeft('') #inserts an empty node as a leftChild
+			#print currentTree
+			pStack.push(currentTree) #pushes current tree into the stack
+			print 'StackSize: {},  root: {}      iteration(push): {} '.format(pStack.size(), currentTree.getRootVal(), i)
+			currentTree = currentTree.getLeftChild() #sets current position to the leftChild
+
+		elif i not in ['+', '-', '*', '/', ')']:
+			print '-----before root: {} '.format(currentTree.getRootVal())
+			currentTree.setRootVal(int(i))  #we are located at the node so set  the root value to the number 
+			#parent = pStack.pop()  #???
+			#print 'StackSize: {},  parent: {}     iteration(pop): digits'.format(pStack.size(), parent.getRootVal())
+			#currentTree = parent  #this sets the current position to the parent node
+			currentTree = pStack.pop()
+			print 'StackSize: {},     iteration(pop): digits'.format(pStack.size())
+			print '-----after root: {} '.format(currentTree.getRootVal())
+
+		elif i in ['+', '-', '*', '/']:
+			currentTree.setRootVal(i) #set the value to the operator for "currentTree"
+			currentTree.insertRight('')  # descend to the right child
+			pStack.push(currentTree) #push the new tree to the stack (will it push last updated node or the whole tree? Will it souble it?)
+			print 'StackSize: {}, root: {}      iteration(push): {}'.format(pStack.size(), currentTree.getRootVal(), i)
+			currentTree = currentTree.getRightChild()  # this set the current position to the rightChild
+
+		elif i == ')':
+			currentTree = pStack.pop()
+			print 'StackSize: {}, root: {}      iteration(pop)'.format(pStack.size(), currentTree.getRootVal(), i)
+
+		else:
+			raise ValueError
+	return eTree
+
+
+def evaluate(parseTree):
+	opers = {'+':operator.add, '-':operator.sub, '*':operator.mul, '/':operator.truediv}
+
+	leftC = parseTree.getLeftChild()
+	rightC = parseTree.getRightChild()
+
+	if leftC and rightC:
+		fn = opers[parseTree.getRootVal()]
+		return fn(evaluate(leftC),evaluate(rightC))
+	else:
+		return parseTree.getRootVal()
+
+
+pt = buildParseTree("( ( 10 + 5 ) * 3 )")
+pt.postorder()  
+print evaluate(pt)
+
+
+
+
